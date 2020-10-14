5240c228bda8c5080e2d6c43356abd64641e25e6
Khanh <khanh371013@gmail.com>
2020-10-14 22:03:31 +0700

    44

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>user id: {{ $route.params.id }}</h1>
+</template>
+ 
---
+++ pages/users/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>View Users</h1>
+  <input type="text">
+  <button>submit</button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
